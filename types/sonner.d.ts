3f228c9b1e0e0e461347eef0d33e056572595ac2
SulthanRaghib <raghib.smp@gmail.com>
2025-11-08 19:04:14 +0700

    Add TypeScript definitions for Sonner toast notifications

---
+++ types/sonner.d.ts
@@ -0,0 +1,8 @@
+declare module "sonner" {
+  export const toast: (...args: any[]) => any
+  export const Toaster: any
+  export default {
+    toast: (...args: any[]) => any,
+    Toaster: any,
+  }
+}
